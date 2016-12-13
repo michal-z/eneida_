e2a0c53048c2b26f392dd2e0c794e32d226b8d71
michal-z <michalziulek@gmail.com>
2016-12-13 17:21:35 +0100

    Added tbb lib.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,13 +2,13 @@
 #include "EASTL/vector.h"
 
 
-void *operator new[](size_t size, const char* /*name*/, int /*flags*/,
+void* operator new[](size_t size, const char* /*name*/, int /*flags*/,
                      unsigned /*debugFlags*/, const char* /*file*/, int /*line*/)
 {
     return malloc(size);
 }
 
-void *operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* /*name*/,
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* /*name*/,
                      int /*flags*/, unsigned /*debugFlags*/, const char* /*file*/, int /*line*/)
 {
     return _aligned_offset_malloc(size, alignment, alignmentOffset);
